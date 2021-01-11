b0d060a3bd76176f1956d948b859ae1d53526e2c
Antonio Sousa <antoniowsj@outlook.com>
2021-01-11 15:38:11 -0300

    Observer design pattern to update tableview - Padrão de design do observador para atualizar tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -22,6 +25,8 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 	
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>(); //comando que permite outros objetos se inscreverem na lista e mudar o evento
 
 	@FXML
 	private TextField txtId;
@@ -46,6 +51,10 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 	
+	public void subscribeDataChangeListener (DataChangeListener listener) {
+		dataChangeListeners.add(listener); //método que permite inscrever outros listeners na lista
+	}
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if (entity == null) {
@@ -54,16 +63,24 @@ public class DepartmentFormController implements Initializable {
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
-		try { /*a operação é feita com banco de dados, e banco de dados pode gerar exceção, por isso, foi criado o bloco try-catch*/
-			entity = getFormData(); /*método que vai ser responsável por pegar os dados na caixa do formulário e instanciar um departamento*/
+		try {
+			entity = getFormData();
 			service.saveOrUpdate(entity);
-			Utils.currenStage(event).close(); /*comando para fechar a janela após salvar um departamento ao pegar a referência da janela atual*/
+			notifyDataChangeListeners();
+			Utils.currenStage(event).close();
 		}
 		catch (DbException dbe) {
 			Alerts.showAlert("Error saving object", null, dbe.getMessage(), AlertType.ERROR);
 		}
 	}
 	
+	private void notifyDataChangeListeners() { //método que vai emitir o evento onDataChanged para outras listas
+		for (DataChangeListener listener : dataChangeListeners) { //para cada DataChangeListener listener pertencente (:) à lista (dataChangeListeners), é preciso fazer um listener chamando o evento onDataChanged
+			listener.onDataChanged();
+		}
+		
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		
@@ -75,7 +92,7 @@ public class DepartmentFormController implements Initializable {
 
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
-		Utils.currenStage(event).close(); /*comando para fechar a janela após cancelar um departamento ao pegar a referência da janela atual*/
+		Utils.currenStage(event).close();
 	}
 	
 
