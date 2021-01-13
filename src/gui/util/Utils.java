bb38e5453fc4095d6db4aaabde4c535db5d47379
Antonio Sousa <antoniowsj@outlook.com>
2021-01-12 21:13:06 -0300

    Seller TableView - Tabela dos Vendedores

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,7 +1,13 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
@@ -11,13 +17,50 @@ public class Utils {
 	}
 	
 	public static Integer tryParseToInt(String str) {
-		try { /*neste caso, um try-catch é criado não para lançar uma exceção, e sim para, quando houver um conteúdo inválido (ex:letras ou conteúdo vazio), lançar um valor nulo*/
-			return Integer.parseInt(str);/*neste caso, ou vai retornar um valor inteiro ou um valor nulo*/
-		}/*serve também para facilitar o processo e evitar de repetir o comando toda vez que ler os dados de uma caixinha de um número inteiro*/
+		try { 
+			return Integer.parseInt(str);
+		}
 		catch (NumberFormatException nfe) {
 			return null;
 		}
 
+	}
+	
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) { //formatTableColumnDate = método utilitário para formatar a data
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+	
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) { //formatTableColumnDate = método utilitário para formatar o número do ponto flutuante
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
 	}
 
 }
